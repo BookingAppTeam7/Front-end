65866fa02251b682391c2267aa4c5740a5712618
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2023-11-27 23:34:29 +0100

    Nav -bar, register and login
    Fixed images, addes animations fade In and fade Out, improved register and login forms.

--- ProjectApp/src/app/register/register-form/register-form.component.ts
+++ ProjectApp/src/app/register/register-form/register-form.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
-
 
 @Component({
   selector: 'app-register-form',
@@ -16,29 +15,38 @@ import {MatSelectModule} from '@angular/material/select';
   imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule,MatIconModule,MatButtonModule,MatSelectModule],
 })
 export class RegisterFormComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  name = new FormControl('', [Validators.required, Validators.requiredTrue]);
-  password = new FormControl('', [Validators.required, Validators.requiredTrue]);
-  surname = new FormControl('', [Validators.required, Validators.requiredTrue]);
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  // name = new FormControl('', [Validators.required, Validators.requiredTrue]);
+  // password = new FormControl('', [Validators.required, Validators.requiredTrue]);
+  // surname = new FormControl('', [Validators.required, Validators.requiredTrue]);
+
+  createRegisterForm=new FormGroup({
+    name: new FormControl(),
+    surname:new FormControl(),
+    phoneNumber: new FormControl(),
+    address: new FormControl(),
+    email: new FormControl(),
+    password:new FormControl()
+  })
   hide = true;
   constructor(private router: Router) { }
   navigateToHome() {
-    // ...
-    this.router.navigate(['home']);
+ 
+   this.router.navigate(['home']);
   }
-  getErrorMailMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
+  // getErrorMailMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-  getErrorEmptyMessage() {
-    if (this.name.hasError('required')|| this.password.hasError('required')|| this.surname.hasError('required')) {
-      return 'You must enter a value';
-    }
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
+  // getErrorEmptyMessage() {
+  //   if (this.name.hasError('required')|| this.password.hasError('required')|| this.surname.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
 
-    return '';
-  }
+  //   return '';
+  // }
 }
 
