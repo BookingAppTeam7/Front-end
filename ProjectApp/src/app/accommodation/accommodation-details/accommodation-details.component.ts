f92bc5b32a8be7f510395d8a65cc07aeb51ab0e1
Luka Petković <lukapetkovicns21@gmail.com>
2023-11-28 19:58:33 +0100

    Fixed accommodation cards

--- ProjectApp/src/app/accommodation/accommodation-details/accommodation-details.component.ts
+++ ProjectApp/src/app/accommodation/accommodation-details/accommodation-details.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Accommodation } from '../accommodation/model/accommodation.model';
 import { ActivatedRoute, ParamMap, Route, Router } from '@angular/router';
 import { AccommodationService } from '../accommodation.service';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-accommodation-details',
   templateUrl: './accommodation-details.component.html',
-  styleUrls: ['./accommodation-details.component.css']
+  styleUrls: ['./accommodation-details.component.css'],
+  standalone:true,
+  imports:[MatChipsModule,MatIconModule,MatInputModule,MatFormFieldModule,MatButtonModule,MatListModule],
 })
 export class AccommodationDetailsComponent implements OnInit{
   accommodation: Accommodation;
--- ProjectApp/src/app/accommodation/accommodation.service.ts
+++ ProjectApp/src/app/accommodation/accommodation.service.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class AccommodationService {
-  private accommodationList: Accommodation[] = [{
+  accommodationList: Accommodation[] = [{
     id: 1,
     name: 'Cozy Cottage',
     description: 'A charming cottage near the countryside.',
@@ -16,7 +16,9 @@ export class AccommodationService {
     minGuests: 2,
     maxGuests: 4,
     status: 'Available',
-    image: '../assets/images/accommodation1.jpg'
+    image: '../../assets/images/accommodation1.jpg',
+    price: 150.0,
+    rating: 4.4
   },
   {
     id: 2,
@@ -26,7 +28,9 @@ export class AccommodationService {
     minGuests: 6,
     maxGuests: 10,
     status: 'Booked',
-    image: '../assets/images/accommodation2.jpg'
+    image: '../../assets/images/accommodation2.jpg',
+    price: 200.0,
+    rating: 3.9
   },
   {
     id: 3,
@@ -36,7 +40,9 @@ export class AccommodationService {
     minGuests: 2,
     maxGuests: 6,
     status: 'Available',
-    image: '../assets/images/accommodation3.jpg'
+    image: '../../assets/images/accommodation3.jpg',
+    price: 175.0,
+    rating: 3.3
   },
   {
     id: 4,
@@ -46,7 +52,9 @@ export class AccommodationService {
     minGuests: 1,
     maxGuests: 3,
     status: 'Available',
-    image: '../assets/images/accommodation4.jpg'
+    image: '../../assets/images/accommodation4.jpg',
+    price: 125.0,
+    rating: 4.7
   },
   {
     id: 5,
@@ -56,8 +64,47 @@ export class AccommodationService {
     minGuests: 2,
     maxGuests: 8,
     status: 'Available',
-    image: '../assets/images/accommodation5.jpg'
-  }];
+    image: '../../assets/images/accommodation5.jpg',
+    price: 160.0,
+    rating: 4.9
+  },
+  {
+    id: 6,
+    name: 'Historic Bed and Breakfast',
+    description: 'A historic B&B with period charm.',
+    location: 'Old Town, United Kingdom',
+    minGuests: 2,
+    maxGuests: 8,
+    status: 'Available',
+    image: '../../assets/images/accommodation5.jpg',
+    price: 160.0,
+    rating: 4.9
+  },
+  {
+    id: 7,
+    name: 'Urban City Loft',
+    description: 'A modern loft in the heart of the city.',
+    location: 'Metropolis City, USA',
+    minGuests: 1,
+    maxGuests: 3,
+    status: 'Available',
+    image: '../../assets/images/accommodation4.jpg',
+    price: 125.0,
+    rating: 4.7
+  },
+  {
+    id: 8,
+    name: 'Mountain Retreat Cabin',
+    description: 'A cozy cabin nestled in the mountains.',
+    location: 'Serenity Peaks, Canada',
+    minGuests: 2,
+    maxGuests: 6,
+    status: 'Available',
+    image: '../../assets/images/accommodation3.jpg',
+    price: 175.0,
+    rating: 3.3
+  }
+];
 
   constructor(private httpClient: HttpClient) {
   }
