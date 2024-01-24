1dba066cd6816cdb5f27f2a44729b51229542243
Luka Petković <lukapetkovicns21@gmail.com>
2024-01-24 17:03:12 +0100

    Front-end README

--- ProjectApp/src/app/auth/guard/auth.guard.ts
+++ ProjectApp/src/app/auth/guard/auth.guard.ts
@@ -27,7 +27,7 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    const userRole :string = this.authService.user$.getValue();
+    const userRole = this.authService.user$.getValue();
 
     if (userRole == null) {
       this.router.navigate(['login']);
